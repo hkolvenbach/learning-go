62c110e7ea5e74f8806126146e4bfa1c83f1d4e6
Hanno Kolvenbach <hannokolvenbach@gmx.de>
2021-09-15 17:58:25 -0600

    use a function
    Signed-off-by: Hanno Kolvenbach <hannokolvenbach@gmx.de>

--- 1-hello-world-go/main.go
+++ 1-hello-world-go/main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hello World")
+	sayHelloWorld("Hello World, again.")
+}
+
+func sayHelloWorld(whatToSay string) {
+	fmt.Println(whatToSay)
 }
