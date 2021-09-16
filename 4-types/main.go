ee961a2e9876433913b8cbc3c78a078a87acdf77
Hanno Kolvenbach <hannokolvenbach@gmx.de>
2021-09-16 17:50:24 -0600

    pointer example
    Signed-off-by: Hanno Kolvenbach <hannokolvenbach@gmx.de>

--- 4-types/main.go
+++ 4-types/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var myInt int = 10
+
+	myFirstPointer := &myInt
+
+	fmt.Println(myInt)
+	fmt.Println("myFirstPointer is", myFirstPointer)
 }
