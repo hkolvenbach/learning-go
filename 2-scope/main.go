1205f757a5d55aa7426ffa206d71e55ca1fc4f80
Hanno Kolvenbach <hannokolvenbach@gmx.de>
2021-09-15 21:15:54 -0600

    cleaned for scope challenge
    Signed-off-by: Hanno Kolvenbach <hannokolvenbach@gmx.de>

--- 2-scope/main.go
+++ 2-scope/main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"myapp/packageone"
-)
-
-var one = "One"
-
 func main() {
-	var one = "this is a block level variable"
-	fmt.Println(one)
 
-	myFunc()
-
-	newString := packageone.PublicVar
-
-	fmt.Println(newString)
-
-	packageone.Exported()
-}
-
-func myFunc() {
-	//var one = "the number one"
-	fmt.Println(one)
 }
--- 2-scope/packageone/package.go
+++ 2-scope/packageone/package.go
@@ -1,10 +1,1 @@
 package packageone
-
-var privateVar = "I am private"
-var PublicVar = "I am Public (or exported)"
-
-func notExported() {
-	//
-}
-
-func Exported() {}
