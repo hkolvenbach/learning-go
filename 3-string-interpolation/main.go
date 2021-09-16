0bc924b87e276710de307c91860412de3ff62983
Hanno Kolvenbach <hannokolvenbach@gmx.de>
2021-09-16 07:49:48 -0600

    add check for name question
    Signed-off-by: Hanno Kolvenbach <hannokolvenbach@gmx.de>

--- 3-string-interpolation/main.go
+++ 3-string-interpolation/main.go
@@ -27,12 +27,18 @@ func prompt() {
 }
 
 func readString(s string) string {
-	fmt.Println(s)
-	prompt()
-	userInput, _ := reader.ReadString('\n')
-	userInput = strings.Replace(userInput, "\r\n", "", -1)
-	userInput = strings.Replace(userInput, "\n", "", -1)
-	return userInput
+	for {
+		fmt.Println(s)
+		prompt()
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+		if userInput == "" {
+			fmt.Println("Please enter a value")
+		} else {
+			return userInput
+		}
+	}
 }
 
 func readInt(s string) int {
