ade7b996153b6ded99dfa3b2ea64fe03038db3c1
Hanno Kolvenbach <hannokolvenbach@gmx.de>
2021-09-16 19:59:19 -0600

    expressions
    Signed-off-by: Hanno Kolvenbach <hannokolvenbach@gmx.de>

--- 4-types/main.go
+++ 4-types/main.go
@@ -2,50 +2,16 @@ package main
 
 import "fmt"
 
-type Animal interface {
-	Says() string
-	HowManyLegs() int
-}
-
-type Dog struct {
-	Name         string
-	Sound        string
-	NumberOfLegs int
-}
-
-func (d *Dog) Says() string {
-	return d.Sound
-}
-
-func (d *Dog) HowManyLegs() int {
-	return d.NumberOfLegs
-}
-
-type Cat struct {
-	Name         string
-	Sound        string
-	NumberOfLegs int
-	HasTail      bool
-}
-
-func (d *Cat) Says() string {
-	return d.Sound
-}
-
-func (d *Cat) HowManyLegs() int {
-	return d.NumberOfLegs
-}
-
 func main() {
-	dog := Dog{Name: "Dog", Sound: "Woof", NumberOfLegs: 4}
-	Riddle(&dog)
+	age := 10
+	name := "Jack"
+	rightHanded := true
 
-	cat := Cat{Name: "Cat", Sound: "Meow", NumberOfLegs: 4, HasTail: true}
+	fmt.Printf("%s is %d years old. Right handed: %t", name, age, rightHanded)
+	fmt.Println()
 
-	Riddle(&cat)
-}
-
-func Riddle(a Animal) {
-	riddle := fmt.Sprintf("This animal says \"%s\" and has %d legs. What animal is it?", a.Says(), a.HowManyLegs())
-	fmt.Println(riddle)
+	// expressions
+	age = age + 10
+	isTeenager := age >= 13
+	fmt.Printf("Is teenager: %t\n", isTeenager)
 }
