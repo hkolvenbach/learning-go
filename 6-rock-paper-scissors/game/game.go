bcdb9dc3389c211ee66786c86bc2b06ab948b09b
Hanno Kolvenbach <hannokolvenbach@gmx.de>
2021-09-17 13:15:27 -0600

    rock paper scissors
    Signed-off-by: Hanno Kolvenbach <hannokolvenbach@gmx.de>

---
+++ 6-rock-paper-scissors/game/game.go
@@ -0,0 +1,148 @@
+package game
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"time"
+)
+
+const (
+	ROCK     = 0
+	PAPER    = 1
+	SCISSORS = 2
+)
+
+type Game struct {
+	DisplayChan chan string
+	RoundChan   chan int
+	Round       Round
+}
+
+type Round struct {
+	RoundNumber   int
+	PlayerScore   int
+	ComputerScore int
+}
+
+var reader = bufio.NewReader(os.Stdin)
+
+func (g *Game) Rounds() {
+	// use select to process input into channels
+	// Print to screen
+	// keep track of round number
+	for {
+		select {
+		case round := <-g.RoundChan:
+			g.Round.RoundNumber = g.Round.RoundNumber + round
+			g.RoundChan <- 1
+		case msg := <-g.DisplayChan:
+			fmt.Println(msg)
+		}
+	}
+}
+
+// clearScreen clears the screen
+func (g *Game) ClearScreen() {
+	if strings.Contains(runtime.GOOS, "windows") {
+		// windows
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else {
+		// linux or mac
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
+func (g *Game) PrintIntro() {
+	fmt.Println("Please enter rock, paper, or scissors")
+}
+
+func (g *Game) PlayRound() bool {
+	rand.Seed(time.Now().UnixNano())
+	playerValue := -1
+	fmt.Println()
+	fmt.Println("Round", g.Round.RoundNumber)
+
+	fmt.Print("Please enter rock, paper, or scissors ->")
+	playerChoice, _ := reader.ReadString('\n')
+	playerChoice = strings.Replace(playerChoice, "\r\n", "", -1)
+	playerChoice = strings.Replace(playerChoice, "\n", "", -1)
+
+	computerValue := rand.Intn(2)
+
+	if playerChoice == "rock" {
+		playerValue = ROCK
+	} else if playerChoice == "paper" {
+		playerValue = PAPER
+	} else if playerChoice == "scissors" {
+		playerValue = SCISSORS
+	}
+
+	fmt.Println()
+	fmt.Println("Player chose", playerChoice)
+	g.DisplayChan <- fmt.Sprintf("Player chose %s", strings.ToUpper(playerChoice))
+
+	switch computerValue {
+	case ROCK:
+		g.DisplayChan <- fmt.Sprintf("Computer chose ROCK")
+		break
+	case PAPER:
+		g.DisplayChan <- fmt.Sprintf("Computer chose PAPER")
+		break
+	case SCISSORS:
+		g.DisplayChan <- fmt.Sprintf("Computer chose SCISSORS")
+		break
+	default:
+	}
+
+	if playerValue == computerValue {
+		g.DisplayChan <- fmt.Sprintf("Its a draw.")
+	} else {
+		switch playerValue {
+		case ROCK:
+			if computerValue == PAPER {
+				g.computerWins()
+			} else {
+				g.playerWins()
+			}
+			break
+		case PAPER:
+			if computerValue == SCISSORS {
+				g.computerWins()
+			} else {
+				g.playerWins()
+			}
+			break
+		case SCISSORS:
+			if computerValue == ROCK {
+				g.computerWins()
+			} else {
+				g.playerWins()
+			}
+			break
+		default:
+			g.DisplayChan <- "Invalid choice"
+			// reset round counter by one
+			return false
+		}
+	}
+	return true
+}
+
+func (g *Game) computerWins() {
+	g.Round.ComputerScore++
+	g.DisplayChan <- "Computer wins!"
+}
+
+func (g *Game) playerWins() {
+	g.Round.PlayerScore++
+	g.DisplayChan <- "Player wins!"
+}
--- 6-rock-paper-scissors/main.go
+++ 6-rock-paper-scissors/main.go
@@ -1,130 +1,46 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"math/rand"
-	"os"
-	"os/exec"
-	"runtime"
-	"strings"
-	"time"
-)
-
-const (
-	ROCK     = 0
-	PAPER    = 1
-	SCISSORS = 2
+	"myapp/game"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	playerChoice := ""
-	playerValue := -1
+	displayChan := make(chan string)
+	roundChan := make(chan int)
+	game := game.Game{
+		DisplayChan: displayChan,
+		RoundChan:   roundChan,
+		Round: game.Round{
+			RoundNumber:   0,
+			PlayerScore:   0,
+			ComputerScore: 0},
+	}
 
-	playerScore := 0
-	computerScore := 0
+	go game.Rounds()
+	game.ClearScreen()
+	game.PrintIntro()
 
-	reader := bufio.NewReader(os.Stdin)
-
-	clearScreen()
-
-	for i := 1; i <= 3; i++ {
-		computerValue := rand.Intn(2)
-
-		fmt.Printf("Round %d:\n", i)
-		fmt.Print("Please enter rock, paper, or scissors ->")
-		playerChoice, _ = reader.ReadString('\n')
-		playerChoice = strings.Replace(playerChoice, "\r\n", "", -1)
-		playerChoice = strings.Replace(playerChoice, "\n", "", -1)
-
-		if playerChoice == "rock" {
-			playerValue = ROCK
-		} else if playerChoice == "paper" {
-			playerValue = PAPER
-		} else if playerChoice == "scissors" {
-			playerValue = SCISSORS
-		} else {
-			// reset if invalid data is entered
-			playerValue = -1
+	for {
+		game.RoundChan <- 1
+		<-game.RoundChan
+		if game.Round.RoundNumber > 3 {
+			break
 		}
 
-		switch computerValue {
-		case ROCK:
-			fmt.Println("Computer chose ROCK")
-			break
-		case PAPER:
-			fmt.Println("Computer chose PAPER")
-			break
-		case SCISSORS:
-			fmt.Println("Computer chose SCISSORS")
-			break
-		default:
-		}
-		fmt.Println("Player value is", playerValue)
-
-		if playerValue == computerValue {
-			fmt.Println("Its a draw.")
-		} else {
-			switch playerValue {
-			case ROCK:
-				if computerValue == PAPER {
-					fmt.Println("Computer wins")
-					computerScore++
-				} else {
-					fmt.Println("Player wins")
-					playerScore++
-				}
-				break
-			case PAPER:
-				if computerValue == SCISSORS {
-					fmt.Println("Computer wins")
-					computerScore++
-				} else {
-					fmt.Println("Player wins")
-					playerScore++
-				}
-				break
-			case SCISSORS:
-				if computerValue == ROCK {
-					fmt.Println("Computer wins")
-					computerScore++
-				} else {
-					fmt.Println("Player wins")
-					playerScore++
-				}
-				break
-			default:
-				fmt.Println("Invalid choice")
-				// reset round counter by one
-				i--
-			}
+		if !game.PlayRound() {
+			game.RoundChan <- -1
+			<-game.RoundChan
 		}
 	}
 
-	fmt.Println("Computer Score:", computerScore)
-	fmt.Println("Player Score:", playerScore)
-	if computerScore == playerScore {
-		fmt.Println("It's a draw!")
-	} else if computerScore > playerScore {
-		fmt.Println("Computer wins!")
-	} else {
-		fmt.Println("Player wins!")
-	}
+	// fmt.Println("Computer Score:", computerScore)
+	// fmt.Println("Player Score:", playerScore)
+	// if computerScore == playerScore {
+	// 	fmt.Println("It's a draw!")
+	// } else if computerScore > playerScore {
+	// 	fmt.Println("Computer wins!")
+	// } else {
+	// 	fmt.Println("Player wins!")
+	// }
 
-}
-
-// clearScreen clears the screen
-func clearScreen() {
-	if strings.Contains(runtime.GOOS, "windows") {
-		// windows
-		cmd := exec.Command("cmd", "/c", "cls")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	} else {
-		// linux or mac
-		cmd := exec.Command("clear")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	}
 }
