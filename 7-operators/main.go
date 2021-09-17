401a2dc4ac5564d16fdcb4e587a922628b351365
Hanno Kolvenbach <hannokolvenbach@gmx.de>
2021-09-17 15:01:14 -0600

    some more operators
    Signed-off-by: Hanno Kolvenbach <hannokolvenbach@gmx.de>

--- 7-operators/main.go
+++ 7-operators/main.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	answer := 7 + 3*4
-	fmt.Println("Answer", answer)
+	// area = pi*r^2
+	var radius = 12.0
+	area := math.Pi * radius * radius
+	fmt.Println("Area", area)
 
-	answer = (7 + 3) * 4
-	fmt.Println("Answer", answer)
+	// division
+	var half float32 = 1.0 / 2.0
+	fmt.Println("half with integer division", half)
+
+	// 3 squared
+	squared := math.Pow(3.0, 2.0)
+	fmt.Println("3^2=", squared)
+
 }
