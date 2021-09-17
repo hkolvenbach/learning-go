e36ccaa43f45af7966058d26b7aeb1f914f70ff6
Hanno Kolvenbach <hannokolvenbach@gmx.de>
2021-09-16 20:06:50 -0600

    combound expressions baseline
    Signed-off-by: Hanno Kolvenbach <hannokolvenbach@gmx.de>

--- 4-types/main.go
+++ 4-types/main.go
@@ -2,16 +2,37 @@ package main
 
 import "fmt"
 
+type Employee struct {
+	Name     string
+	Age      int
+	Salary   int
+	FullTime bool
+}
+
 func main() {
-	age := 10
-	name := "Jack"
-	rightHanded := true
+	jack := Employee{
+		Name:     "Jack Smith",
+		Age:      27,
+		Salary:   40000,
+		FullTime: false,
+	}
 
-	fmt.Printf("%s is %d years old. Right handed: %t", name, age, rightHanded)
-	fmt.Println()
+	jill := Employee{
+		Name:     "Jill Jones",
+		Age:      33,
+		Salary:   60000,
+		FullTime: true,
+	}
 
-	// expressions
-	age = age + 10
-	isTeenager := age >= 13
-	fmt.Printf("Is teenager: %t\n", isTeenager)
+	var employees []Employee
+	employees = append(employees, jack)
+	employees = append(employees, jill)
+
+	for _, x := range employees {
+		if x.Age > 30 {
+			fmt.Println(x.Name, "is older than 30.")
+		} else {
+			fmt.Println(x.Name, "is under 30.")
+		}
+	}
 }
