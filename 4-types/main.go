7d202d5d4a080392d74a3606edec8aced9fa0c3b
Hanno Kolvenbach <hannokolvenbach@gmx.de>
2021-09-16 18:16:49 -0600

    show attached function
    Signed-off-by: Hanno Kolvenbach <hannokolvenbach@gmx.de>

--- 4-types/main.go
+++ 4-types/main.go
@@ -2,24 +2,16 @@ package main
 
 import "fmt"
 
-func main() {
-	var animals []string
-	animals = append(animals, "dog")
-	animals = append(animals, "cat")
-
-	fmt.Println(animals)
-
-	for _, x := range animals {
-		fmt.Println(x)
-	}
+type Animal struct {
+	Name  string
+	Sound string
 }
 
-func DeleteFromSlice(a []string, i int) {
-	// copy last element to ith element (which will be deleted)
-	// this will not keep the order
-	a[i] = a[len(a)-1]
-	// set last element to nothing
-	a[len(a)-1] = ""
-	// use 0 to len(a)-1 elements and assign to a
-	a = a[:len(a)-1]
+func main() {
+	myAnimal := Animal{Name: "dog", Sound: "woof"}
+	myAnimal.Says()
+}
+
+func (a *Animal) Says() {
+	fmt.Println("Animal", a.Name, "says", a.Sound)
 }
