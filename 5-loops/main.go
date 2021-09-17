dd9f10edac25f285ce04fc729fb0c2f9d218375d
Hanno Kolvenbach <hannokolvenbach@gmx.de>
2021-09-17 07:21:54 -0600

    conditional while
    Signed-off-by: Hanno Kolvenbach <hannokolvenbach@gmx.de>

--- 5-loops/main.go
+++ 5-loops/main.go
@@ -1,9 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	for i := 10; i >= 0; i-- {
-		fmt.Println("i is", i)
+	rand.Seed(time.Now().UnixNano())
+	i := 1000
+	for i > 100 {
+		// get random number
+		i = rand.Intn(1000) + 1
+		fmt.Println("I is", i)
+		if i > 100 {
+			fmt.Println("is is", i, "so loop keeps going")
+		}
 	}
+	fmt.Println("Got", i, "and broke out of the loop")
 }
